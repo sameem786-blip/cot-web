f562b9a34f5020b4e574f93e0f6408be76c4f83c
Athar Changezi <sameemabbas2002@outlook.com>
2024-07-23 04:34:12 +0500

    features set

--- src/types/feature.ts
+++ src/types/feature.ts
@@ -2,5 +2,11 @@ export type Feature = {
   id: number;
   icon: JSX.Element;
   title: string;
+  service_a: String,
+  service_b: String,
+  service_c: String,
+  service_d?: String,
+  service_e?: String,
+  service_f?: String,
   paragraph: string;
 };
