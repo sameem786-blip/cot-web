c0b9a32f5e265851c26991800a71acb43653a93a
Athar Changezi <sameemabbas2002@outlook.com>
2024-07-26 04:24:49 +0500

    hello a

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { useTheme } from "next-themes";
+
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const { theme, setTheme } = useTheme();
+  setTheme("dark")
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
