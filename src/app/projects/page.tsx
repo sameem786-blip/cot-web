9a1cfb933708a3778c1cebc12a1d704515f50498
Athar Changezi <sameemabbas2002@outlook.com>
2024-07-28 01:43:04 +0500

    proejcts page set

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -20,16 +20,13 @@ const Blog = () => {
 
       <section className="pb-[120px] pt-[120px]">
         <div className="container">
-          <div className="-mx-4 flex flex-wrap justify-center">
-            {blogData.map((blog) => (
-              <div
-                key={blog.id}
-                className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
-              >
-                <SingleBlog blog={blog} />
-              </div>
-            ))}
-          </div>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8">
+          {blogData.map((blog) => (
+            <div key={blog.id} className="w-full">
+              <SingleBlog blog={blog} />
+            </div>
+          ))}
+        </div>
 
           <div className="-mx-4 flex flex-wrap" data-wow-delay=".15s">
             <div className="w-full px-4">
