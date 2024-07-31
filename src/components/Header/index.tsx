21daf0980288867a9c0081278eb965cef1b9a089
Sameem Abbas <sameemabbas2002@outlook.com>
2024-08-01 02:00:39 +0500

    navbar setting

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -24,26 +24,25 @@ const Header = () => {
   };
   useEffect(() => {
     window.addEventListener("scroll", handleStickyNavbar);
-  });
+    return () => {
+      window.removeEventListener("scroll", handleStickyNavbar);
+    };
+  }, []);
 
-  // submenu handler
+  // Submenu handler
   const [openIndex, setOpenIndex] = useState(-1);
   const handleSubmenu = (index) => {
-    if (openIndex === index) {
-      setOpenIndex(-1);
-    } else {
-      setOpenIndex(index);
-    }
+    setOpenIndex(openIndex === index ? -1 : index);
   };
 
-  const usePathName = usePathname();
+  const pathname = usePathname();
 
   return (
     <>
       <header
         className={`header left-0 top-0 z-40 flex w-full items-center ${
           sticky
-            ? "dark:bg-gray-dark dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
+            ? "fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition dark:bg-gray-dark dark:shadow-sticky-dark"
             : "absolute bg-transparent"
         }`}
       >
@@ -111,7 +110,7 @@ const Header = () => {
                           <Link
                             href={menuItem.path}
                             className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
-                              usePathName === menuItem.path
+                              pathname === menuItem.path
                                 ? "text-primary dark:text-white"
                                 : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
                             }`}
@@ -156,14 +155,220 @@ const Header = () => {
                       </li>
                     ))}
                   </ul>
+                  {/* Social Icons in Mobile Menu */}
+                  <div className="mt-4 flex items-center lg:hidden">
+                    <a
+                      href="https://www.facebook.com/ComputingOfTomorrow/"
+                      aria-label="social-link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                    >
+                      <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 22 22"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12.1 10.4939V7.42705C12.1 6.23984 13.085 5.27741 14.3 5.27741H16.5V2.05296L13.5135 1.84452C10.9664 1.66676 8.8 3.63781 8.8 6.13287V10.4939H5.5V13.7183H8.8V20.1667H12.1V13.7183H15.4L16.5 10.4939H12.1Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </a>
+                    <a
+                      href="https://www.facebook.com/ComputingOfTomorrow/"
+                      aria-label="social-link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                    >
+                      <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 24 24"
+                        className="fill-current"
+                      >
+                        <path d="M12 2.163c3.204 0 3.584.012 4.849.07 1.259.058 2.123.272 2.627.57.601.346 1.036.83 1.495 1.495.298.504.512 1.368.57 2.627.058 1.265.07 1.645.07 4.849s-.012 3.584-.07 4.849c-.058 1.259-.272 2.123-.57 2.627-.346.601-.83 1.036-1.495 1.495-.504.298-1.368.512-2.627.57-1.265.058-1.645.07-4.849.07s-3.584-.012-4.849-.07c-1.259-.058-2.123-.272-2.627-.57-.601-.346-1.036-.83-1.495-1.495-.298-.504-.512-1.368-.57-2.627-.058-1.265-.07-1.645-.07-4.849s.012-3.584.07-4.849c.058-1.259.272-2.123.57-2.627.346-.601.83-1.036 1.495-1.495.504-.298 1.368-.512 2.627-.57C8.416 2.175 8.796 2.163 12 2.163zm0-2.163C8.741 0 8.332.013 7.052.073 5.773.132 4.61.355 3.603.847 2.571 1.35 1.77 2.15 1.267 3.182.775 4.19.552 5.353.493 6.632.433 7.912.42 8.322.42 12s.013 4.088.073 5.368c.059 1.279.282 2.442.774 3.45.503 1.032 1.303 1.833 2.335 2.335 1.008.492 2.171.715 3.45.774 1.28.06 1.69.073 5.368.073s4.088-.013 5.368-.073c1.279-.059 2.442-.282 3.45-.774 1.032-.503 1.833-1.303 2.335-2.335.492-1.008.715-2.171.774-3.45.06-1.28.073-1.69.073-5.368s-.013-4.088-.073-5.368c-.059-1.279-.282-2.442-.774-3.45-.503-1.032-1.303-1.833-2.335-2.335C19.39.355 18.227.132 16.948.073 15.668.013 15.259 0 12 0zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.162 6.162 6.162 6.162-2.759 6.162-6.162-2.759-6.162-6.162-6.162zm0 10.1c-2.172 0-3.938-1.766-3.938-3.938s1.766-3.938 3.938-3.938 3.938 1.766 3.938 3.938-1.766 3.938-3.938 3.938zm6.406-11.846c-.796 0-1.44-.645-1.44-1.44s.645-1.44 1.44-1.44 1.44.645 1.44 1.44-.645 1.44-1.44 1.44z" />
+                      </svg>
+                    </a>
+                    {/* <a
+                    href="/"
+                    aria-label="social-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                  >
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 22 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M13.9831 19.25L9.82094 13.3176L4.61058 19.25H2.40625L8.843 11.9233L2.40625 2.75H8.06572L11.9884 8.34127L16.9034 2.75H19.1077L12.9697 9.73737L19.6425 19.25H13.9831ZM16.4378 17.5775H14.9538L5.56249 4.42252H7.04674L10.808 9.6899L11.4584 10.6039L16.4378 17.5775Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </a> */}
+                    <a
+                      href="https://www.instagram.com/cot_pak/"
+                      aria-label="social-link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                    >
+                      <svg
+                        width="17"
+                        height="16"
+                        viewBox="0 0 17 16"
+                        className="fill-current"
+                      >
+                        <path d="M15.2196 0H1.99991C1.37516 0 0.875366 0.497491 0.875366 1.11936V14.3029C0.875366 14.8999 1.37516 15.4222 1.99991 15.4222H15.1696C15.7943 15.4222 16.2941 14.9247 16.2941 14.3029V1.09448C16.3441 0.497491 15.8443 0 15.2196 0ZM5.44852 13.1089H3.17444V5.7709H5.44852V13.1089ZM4.29899 4.75104C3.54929 4.75104 2.97452 4.15405 2.97452 3.43269C2.97452 2.71133 3.57428 2.11434 4.29899 2.11434C5.02369 2.11434 5.62345 2.71133 5.62345 3.43269C5.62345 4.15405 5.07367 4.75104 4.29899 4.75104ZM14.07 13.1089H11.796V9.55183C11.796 8.7061 11.771 7.58674 10.5964 7.58674C9.39693 7.58674 9.222 8.53198 9.222 9.47721V13.1089H6.94792V5.7709H9.17202V6.79076H9.19701C9.52188 6.19377 10.2466 5.59678 11.3711 5.59678C13.6952 5.59678 14.12 7.08925 14.12 9.12897V13.1089H14.07Z" />
+                      </svg>
+                    </a>
+                    <a
+                      href="mailto:info@cot.com.pk"
+                      aria-label="social-link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                    >
+                      <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6Z"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M22 6L12 13L2 6"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </a>
+                  </div>
                 </nav>
               </div>
-              <div className="flex items-center justify-end pr-16 lg:pr-0">
-                
-                
-                {/* <div>
-                  <ThemeToggler />
-                </div> */}
+              <div className="hidden lg:flex lg:items-center lg:gap-x-8">
+                <a
+                  href="https://www.facebook.com/ComputingOfTomorrow/"
+                  aria-label="social-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 22 22"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12.1 10.4939V7.42705C12.1 6.23984 13.085 5.27741 14.3 5.27741H16.5V2.05296L13.5135 1.84452C10.9664 1.66676 8.8 3.63781 8.8 6.13287V10.4939H5.5V13.7183H8.8V20.1667H12.1V13.7183H15.4L16.5 10.4939H12.1Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
+                <a
+                  href="https://www.facebook.com/ComputingOfTomorrow/"
+                  aria-label="social-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    className="fill-current"
+                  >
+                    <path d="M12 2.163c3.204 0 3.584.012 4.849.07 1.259.058 2.123.272 2.627.57.601.346 1.036.83 1.495 1.495.298.504.512 1.368.57 2.627.058 1.265.07 1.645.07 4.849s-.012 3.584-.07 4.849c-.058 1.259-.272 2.123-.57 2.627-.346.601-.83 1.036-1.495 1.495-.504.298-1.368.512-2.627.57-1.265.058-1.645.07-4.849.07s-3.584-.012-4.849-.07c-1.259-.058-2.123-.272-2.627-.57-.601-.346-1.036-.83-1.495-1.495-.298-.504-.512-1.368-.57-2.627-.058-1.265-.07-1.645-.07-4.849s.012-3.584.07-4.849c.058-1.259.272-2.123.57-2.627.346-.601.83-1.036 1.495-1.495.504-.298 1.368-.512 2.627-.57C8.416 2.175 8.796 2.163 12 2.163zm0-2.163C8.741 0 8.332.013 7.052.073 5.773.132 4.61.355 3.603.847 2.571 1.35 1.77 2.15 1.267 3.182.775 4.19.552 5.353.493 6.632.433 7.912.42 8.322.42 12s.013 4.088.073 5.368c.059 1.279.282 2.442.774 3.45.503 1.032 1.303 1.833 2.335 2.335 1.008.492 2.171.715 3.45.774 1.28.06 1.69.073 5.368.073s4.088-.013 5.368-.073c1.279-.059 2.442-.282 3.45-.774 1.032-.503 1.833-1.303 2.335-2.335.492-1.008.715-2.171.774-3.45.06-1.28.073-1.69.073-5.368s-.013-4.088-.073-5.368c-.059-1.279-.282-2.442-.774-3.45-.503-1.032-1.303-1.833-2.335-2.335C19.39.355 18.227.132 16.948.073 15.668.013 15.259 0 12 0zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.162 6.162 6.162 6.162-2.759 6.162-6.162-2.759-6.162-6.162-6.162zm0 10.1c-2.172 0-3.938-1.766-3.938-3.938s1.766-3.938 3.938-3.938 3.938 1.766 3.938 3.938-1.766 3.938-3.938 3.938zm6.406-11.846c-.796 0-1.44-.645-1.44-1.44s.645-1.44 1.44-1.44 1.44.645 1.44 1.44-.645 1.44-1.44 1.44z" />
+                  </svg>
+                </a>
+                {/* <a
+                    href="/"
+                    aria-label="social-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                  >
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 22 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M13.9831 19.25L9.82094 13.3176L4.61058 19.25H2.40625L8.843 11.9233L2.40625 2.75H8.06572L11.9884 8.34127L16.9034 2.75H19.1077L12.9697 9.73737L19.6425 19.25H13.9831ZM16.4378 17.5775H14.9538L5.56249 4.42252H7.04674L10.808 9.6899L11.4584 10.6039L16.4378 17.5775Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </a> */}
+                <a
+                  href="https://www.instagram.com/cot_pak/"
+                  aria-label="social-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                >
+                  <svg
+                    width="17"
+                    height="16"
+                    viewBox="0 0 17 16"
+                    className="fill-current"
+                  >
+                    <path d="M15.2196 0H1.99991C1.37516 0 0.875366 0.497491 0.875366 1.11936V14.3029C0.875366 14.8999 1.37516 15.4222 1.99991 15.4222H15.1696C15.7943 15.4222 16.2941 14.9247 16.2941 14.3029V1.09448C16.3441 0.497491 15.8443 0 15.2196 0ZM5.44852 13.1089H3.17444V5.7709H5.44852V13.1089ZM4.29899 4.75104C3.54929 4.75104 2.97452 4.15405 2.97452 3.43269C2.97452 2.71133 3.57428 2.11434 4.29899 2.11434C5.02369 2.11434 5.62345 2.71133 5.62345 3.43269C5.62345 4.15405 5.07367 4.75104 4.29899 4.75104ZM14.07 13.1089H11.796V9.55183C11.796 8.7061 11.771 7.58674 10.5964 7.58674C9.39693 7.58674 9.222 8.53198 9.222 9.47721V13.1089H6.94792V5.7709H9.17202V6.79076H9.19701C9.52188 6.19377 10.2466 5.59678 11.3711 5.59678C13.6952 5.59678 14.12 7.08925 14.12 9.12897V13.1089H14.07Z" />
+                  </svg>
+                </a>
+                <a
+                  href="mailto:info@cot.com.pk"
+                  aria-label="social-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6Z"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M22 6L12 13L2 6"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
