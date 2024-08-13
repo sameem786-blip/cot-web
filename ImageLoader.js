e880ef6b3a4e44bca09c0a8bc26737ad98d60096
Sameem Abbas <sameemabbas2002@outlook.com>
2024-08-14 04:12:22 +0500

    img urls set

--- ImageLoader.js
+++ ImageLoader.js
@@ -1,3 +1,3 @@
 export default function ImageLoader({ src }) {
-  return `http://cot.com.pk.s3-website-us-east-1.amazonaws.com/${src}`;
+  return `http://cot.com.pk.s3-website-us-east-1.amazonaws.com${src}`;
 }
