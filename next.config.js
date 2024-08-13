0d8fe13c2853f7fe17f8e64f74fa1186bb820eaf
Sameem Abbas <sameemabbas2002@outlook.com>
2024-08-14 03:39:17 +0500

    config ssetup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    domains: ["localhost"],
+    domains: [
+      "localhost",
+      "http://cot.com.pk.s3-website-us-east-1.amazonaws.com",
+    ],
     remotePatterns: [
       {
         protocol: "https",
