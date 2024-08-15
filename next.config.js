5104f9bc3c42bd6a2e918c14e242c8f467c7c967
Sameem Abbas <sameemabbas2002@outlook.com>
2024-08-15 14:04:37 +0500

    reset

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    domains: [
-      "localhost",
-      "http://cot.com.pk.s3-website-us-east-1.amazonaws.com",
-    ],
-    loader: "custom",
-    loaderFile: "./ImageLoader.js",
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
