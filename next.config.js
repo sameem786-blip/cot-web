01575e74bc02d8537ea53e992e7b2e2c941b72c7
Sameem Abbas <sameemabbas2002@outlook.com>
2024-08-15 18:49:49 +0500

    wallah

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true, // Enables the faster SWC minifier
+    images: {
+      domains: ['http://localhost','http://52.23.225.133'], // Allow images from specific domains
+    },
+    i18n: {
+      locales: ['en', 'es'], // Specify the supported locales
+      defaultLocale: 'en', // Set the default locale
+    },
+    // Other configuration options here...
+  };
+  
+  module.exports = nextConfig;
